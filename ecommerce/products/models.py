711874060cba6202486bf3ff7bbdb1d20a5478c3
Suresh Thapa <sureshthapa6063@gmail.com>
2025-08-24 21:18:16 +0545

    add role in login response

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 class Category(models.Model):
@@ -23,4 +26,5 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
 
